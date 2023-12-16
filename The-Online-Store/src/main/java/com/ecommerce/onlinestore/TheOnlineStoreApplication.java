22cf90daa29dad6209f283e0763689fdeb32e683
Pratik Sonawane <134759546+prats515@users.noreply.github.com>
2023-12-16 18:30:16 +0530

    Add files via upload

---
+++ The-Online-Store/src/main/java/com/ecommerce/onlinestore/TheOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ The-Online-Store/src/test/java/com/ecommerce/onlinestore/TheOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
